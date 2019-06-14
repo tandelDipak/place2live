c62eb84a9dd801b2b71e781c46c68539c65bc83a
haksoat <shopejuh@gmail.com>
2019-06-14 20:48:53 +0100

    configure traffic spider and output file

--- traffic_index/traffic_index/settings.py
+++ traffic_index/traffic_index/settings.py
@@ -88,3 +88,14 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_EXPORT_FIELDS = (
+    "world_rank",
+    "city",
+    "country",
+    "congestion_level"
+)
+
+LOG_LEVEL = "INFO"
+FEED_FORMAT = "csv"
+FEED_URI = "output/list_of_countries.csv"
