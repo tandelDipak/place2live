eb06dfa15a7e76af47f95b344df92c7e7697135e
haksoat <shopejuh@gmail.com>
2019-06-14 20:51:15 +0100

    Parse contents of url

--- traffic_index/traffic_index/spiders/traffic.py
+++ traffic_index/traffic_index/spiders/traffic.py
@@ -8,4 +8,15 @@ class TrafficSpider(scrapy.Spider):
     start_urls = ["https://www.tomtom.com/en_gb/traffic-index/ranking/"]
 
     def parse(self, response):
-        pass
+        xpath_selector = "//*[@id='RankingPage-table']//table//tr/td[{}]"
+        world_ranks = response.xpath(xpath_selector.format(1)).getall()
+        cities = response.xpath(xpath_selector.format(3)).getall()
+        countries = response.xpath(xpath_selector.format(4)).getall()
+        congestion_levels = response.xpath(xpath_selector.format(5)).getall()
+        for rank, city, country, level in zip(world_ranks, cities, countries, congestion_levels):
+            i = ItemLoader(item=TrafficIndexItem())
+            i.add_value("world_rank", rank)
+            i.add_value("city", city)
+            i.add_value("country", country)
+            i.add_value("congestion_level", level)
+            yield i.load_item()
