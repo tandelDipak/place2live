6eb0ff6fa8a7aa7c1a7266c7a76c9495afcbccff
serhii <aserhii@protonmail.com>
2019-07-18 23:10:28 +0300

    Fix english error :pencil:

--- where.py
+++ where.py
@@ -12,27 +12,28 @@ your_purchasing_power_index = float(
 )
 print(f"In your country purchasing power index is {your_purchasing_power_index}")
 your_purchasing_power_index = (
-    float(input("What is your purchasing power index (higher is better)? "))
+    float(input("What is your desirable purchasing power index (higher is better)? "))
     or your_purchasing_power_index
 )
 
 your_safety_index = float(df[df.country == your_country]["safety_index"])
 print(f"In your country safety index is {your_safety_index}")
 your_safety_index = (
-    float(input("What is your safety index (higher is better)? ")) or your_safety_index
+    float(input("What is your desirable safety index (higher is better)? "))
+    or your_safety_index
 )
 
 your_health_care_index = float(df[df.country == your_country]["health_care_index"])
 print(f"In your country health care index is {your_health_care_index}")
 your_health_care_index = (
-    float(input("What is your health care index (higher is better)? "))
+    float(input("What is your desirable health care index (higher is better)? "))
     or your_health_care_index
 )
 
 your_climate_index = float(df[df.country == your_country]["climate_index"])
 print(f"In your country climate index is {your_climate_index}")
 your_climate_index = (
-    float(input("What is your climate index (higher is better)? "))
+    float(input("What is your desirable climate index (higher is better)? "))
     or your_climate_index
 )
 
@@ -41,7 +42,7 @@ your_cost_of_living_index = float(
 )
 print(f"In your country cost of living index is {your_cost_of_living_index}")
 your_cost_of_living_index = (
-    float(input("What is your cost of living index (lower is better)? "))
+    float(input("What is your desirable cost of living index (lower is better)? "))
     or your_cost_of_living_index
 )
 
@@ -53,7 +54,9 @@ print(
     f"price to income ratio index is {your_property_price_to_income_ratio}"
 )
 your_property_price_to_income_ratio = (
-    float(input("What is your house price to income ratio (lower is better)? "))
+    float(
+        input("What is your desirable house price to income ratio (lower is better)? ")
+    )
     or your_property_price_to_income_ratio
 )
 
@@ -64,14 +67,16 @@ print(
     f"In your country traffic commute time index is {your_traffic_commute_time_index}"
 )
 your_traffic_commute_time_index = (
-    float(input("What is your traffic commute time index (lower is better)? "))
+    float(
+        input("What is your desirable traffic commute time index (lower is better)? ")
+    )
     or your_traffic_commute_time_index
 )
 
 your_pollution_index = float(df[df.country == your_country]["pollution_index"])
 print(f"In your country pollution index is {your_pollution_index}")
 your_pollution_index = (
-    float(input("What is your pollution index (lower is better)? "))
+    float(input("What is your desirable pollution index (lower is better)? "))
     or your_pollution_index
 )
 
@@ -111,15 +116,11 @@ out_df = df[df.purchasing_power_index > your_purchasing_power_index][
     df.climate_index > your_climate_index
 ]
 print_out_df = out_df[
-    [
-        "country",
-        "freedomhouse_score",
-        "quality_of_life_index"
-    ]
+    ["country", "freedomhouse_score", "quality_of_life_index"]
 ].dropna()
 
 if print_out_df.empty:
-    print(f"Don't exist country better any {your_country}.")
+    print(f"There is no country better than {your_country}.")
 else:
     with pd.option_context("display.max_rows", None, "display.max_columns", None):
         print(print_out_df)
