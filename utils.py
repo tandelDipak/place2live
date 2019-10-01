bb2f400c3989edd383055398145146e423c6fea9
rosdyana <rosdyana.kusuma@gmail.com>
2019-10-01 13:32:10 +0800

    fix redefined builtin type

--- utils.py
+++ utils.py
@@ -8,11 +8,11 @@ class text_type(Enum):
     ANSWER = "answer"
 
 
-def text_color(text, type=None):
-    if type == text_type.WARNING:
+def text_color(text, text_type=None):
+    if text_type == text_type.WARNING:
         return cf.red(text)
-    if type == text_type.QUESTION:
+    if text_type == text_type.QUESTION:
         return cf.magenta(text)
-    if type == text_type.ANSWER:
+    if text_type == text_type.ANSWER:
         return cf.green(text)
     return text
