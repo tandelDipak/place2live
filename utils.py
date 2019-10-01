62786289592fba3cd5d45e19d564b57cb294a340
rosdyana <rosdyana.kusuma@gmail.com>
2019-10-01 13:38:28 +0800

    add docstring and fix redefine var name from outer scope

--- utils.py
+++ utils.py
@@ -3,16 +3,18 @@ from enum import Enum
 
 
 class text_type(Enum):
+    """Enum for text type."""
     WARNING = "warning"
     QUESTION = "question"
     ANSWER = "answer"
 
 
-def text_color(text, text_type=None):
-    if text_type == text_type.WARNING:
+def text_color(text, txtType=None):
+    """Return colored text based on text type."""
+    if txtType == text_type.WARNING:
         return cf.red(text)
-    if text_type == text_type.QUESTION:
+    if txtType == text_type.QUESTION:
         return cf.magenta(text)
-    if text_type == text_type.ANSWER:
+    if txtType == text_type.ANSWER:
         return cf.green(text)
     return text
