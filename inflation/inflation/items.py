27584a2cc4042f8c77f8c13117cce7c8f857b541
sebach1 <sebachamena@gmail.com>
2019-10-03 23:15:37 -0300

    Adds inflation crawl

---
+++ inflation/inflation/items.py
@@ -0,0 +1,29 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Field, Item
+from scrapy.loader.processors import MapCompose
+from w3lib.html import remove_tags
+
+
+def parse_string(value):
+    end = value.index("%")
+    return value[: end + 1]
+
+def parse_int(value):
+    return int(value)
+
+def parse_float(value):
+    return float(value)
+
+def strip_string(value):
+    return value.strip()
+
+class InflationItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    inflation = Field()
+    country = Field(input_processor=MapCompose(strip_string))
+    year = Field(input_processor=MapCompose(parse_int))
---
+++ inflation/inflation/spiders/inflation.py
@@ -0,0 +1,36 @@
+import re
+from bs4 import BeautifulSoup as bs
+from scrapy import Spider
+from scrapy.loader import ItemLoader
+from inflation.items import InflationItem
+
+def isRate(candidate):
+    try:
+        float(candidate)
+        return True
+    except Exception:
+        return False
+
+class InflationSpider(Spider):
+    name = "inflation"
+    allowed_domains = ["wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/List_of_countries_by_inflation_rate"]
+
+    def parse(self, response):
+        table = bs(response.body, "html.parser").findAll("tbody")[2]
+
+        countries = [c["title"] for c in table.findAll("a", href=True, title=True)]
+        dates = [re.findall('\d+', yr)[0] for yr in table.findAll("td", {"data-sort-value": True})]
+        inflations = []
+        for td in [i.replace("−", "-") for i in table.findAll("td")]:
+            if isRate(td):
+                inflations.append(td)
+
+        for inflation, country, date in zip(
+            inflations, countries, dates
+        ):
+            i = ItemLoader(item=InflationItem())
+            i.add_value("inflation", inflation)
+            i.add_value("date", date)
+            i.add_value("country", country)
+            yield i.load_item()
