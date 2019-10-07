b2221723b1f3b86eb9399d94f023c54de10c0a45
Paul <amatokus8669@gmail.com>
2019-10-07 20:39:44 +0200

    Cleaning script + pre-commit cleanned other files

--- country_data_grabber/country_data_grabber.py
+++ country_data_grabber/country_data_grabber.py
@@ -1,31 +1,47 @@
-import pandas as pd
 import os
+
+import pandas as pd
 
 
 def getCSVsAsList():
+    """
+    get all parsable CSV in project as full path
+
+    :return: string list
+    """
     fileList = []
     # CSVs to skip because country name is not a PK
-    csvSkipList=[
+    csvSkipList = [
         "university_rankings.csv",
-        "traffic_index.csv"
+        "traffic_index.csv",
     ]
-    for root, dirs, files in os.walk(".."):
+    for root, _unuseddirs, files in os.walk(".."):
         for name in files:
-            if name.find(".csv") != -1 and not(any(f in name for f in csvSkipList)):
+            if name.find(".csv") != -1 and not (any(f in name for f in csvSkipList)):
                 fileList.append(root + "\\" + name)
 
     return fileList
 
 
-def findIndexOfElementInList(list, elements):
-    for i in range(len(list)):
-        e = list[i]
+def findElementInList(passedList, elements):
+    """
+
+    :param passedList: column names of a data frame
+    :param elements: elements to look for in the column names
+    :return: index of first element found in the data frame column names
+    """
+    for i, item in enumerate(passedList):
         for element in elements:
-            if str(e).lower() == str(element).lower():
+            if str(item).lower() == str(element).lower():
                 return i
 
 
 def grabData(countryName):
+    """
+
+    :param countryName: country name as string (case insensitive)
+    :return: data frame with country data
+    """
     CSVs = getCSVsAsList()
     df = pd.DataFrame()
     for CSV in CSVs:
@@ -33,7 +49,9 @@ def grabData(countryName):
         data = pd.read_csv(CSV)
         # column names used to describe the country names
         columnNames = ["country", "name"]
-        country_col_index = findIndexOfElementInList(list(data.columns.values), columnNames)
+        # column names of the data frame
+        dataColumns = list(data.columns.values)
+        country_col_index = findElementInList(dataColumns, columnNames)
         colName = data.columns[country_col_index]
         whereCountry = data[colName].str.lower() == countryName.lower()
         data = data.where(whereCountry).dropna()
@@ -42,9 +60,12 @@ def grabData(countryName):
             df = data
             key = colName
         else:
-            df = df.join(data.set_index([colName], verify_integrity=True),
-                         on=[key], how='left')
+            df = df.join(
+                data.set_index([colName], verify_integrity=True),
+                on=[key], how='left',
+            )
 
-    return(df)
+    return df
+
 
 grabData("france")
